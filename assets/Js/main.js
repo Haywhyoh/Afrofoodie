3f7b636ff13f19f032bfacd673d7be664ceba442
Haywhyoh <adefeyisayo998@gmail.com>
2021-07-22 07:32:09 +0100

    ''

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -2,8 +2,29 @@ const NavMenu = document.getElementById('nav_container'),
       NavToggle = document.getElementById('nav_toggle'),
       NavClose = document.getElementById('nav_close')
 
-if (NavToggle) {
+
     NavToggle.addEventListener('click', () =>{
-        NavMenu.style.visibility ='visible'
+       NavToggle ? NavMenu.style.display ='initial' : NavMenu.style.display='none'
     })
-}
+
+    NavClose.addEventListener('click', () =>{
+        
+        NavClose ? NavMenu.style.display = 'none' : NavMenu.style.display = 'initial'
+    })
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
